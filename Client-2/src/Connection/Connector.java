ee0f79e176d47d2b5c237075aac948299f9a18be
Pontus <guslaepo@student.gu.se>
2017-06-21 15:18:24 -0400

    Add server information

--- Client-2/src/Connection/Connector.java
+++ Client-2/src/Connection/Connector.java
@@ -23,28 +23,42 @@ package Connection;
 	* WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
+import External.Singleton;
+
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class Connector {
 
+	private Socket socket;
+	private boolean connected = false;
+
 	private Socket connect() throws Exception {
-		String sentence;
-		String modifiedSentence;
-		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
 		Socket clientSocket = new Socket("localhost", 6789);
+	}
+
+	private boolean ok(String str) {
 		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
 		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 		sentence = inFromUser.readLine();
 		outToServer.writeBytes(sentence + '\n');
+
 		modifiedSentence = inFromServer.readLine();
 		System.out.println("FROM SERVER: " + modifiedSentence);
-		clientSocket.close();
 	}
 
-	private void send() {
+	private void done() {
+		try {
+			this.socket.close();
+		} catch (IOException ex){
+			Singleton.debugPrint("An Error was thrown when closing the connection socket.");
+		}
+	}
+
+	private void send(String str) {
 
 	}
 
