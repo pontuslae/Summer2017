a945bcc8cc173c096acc413b54b48cf805013be7
Pontus <guslaepo@student.gu.se>
2017-06-21 17:29:35 -0400

    Introduce server mocking

--- Client-2/src/Connection/IgnoreThisTCP.java
+++ Client-2/src/Connection/IgnoreThisTCP.java
@@ -1,8 +1,23 @@
 package Connection;
 
+import External.Singleton;
+
+import java.io.IOException;
 import java.net.*;
 
 class IgnoreThisTCP {
+
+
+	public void mock(String receive, String send){
+		System.out.println("Listening...");
+		try {
+			Socket socket = new ServerSocket(9005).accept();
+		} catch (IOException ex) {
+			Singleton.debugPrint("Exception was thrown during mocking", ex);
+		}
+		System.out.println("Connected");
+	}
+
 	public static void main(String argv[]) throws Exception {
 		System.out.println("Listening...");
 		new ServerSocket(9005).accept();
