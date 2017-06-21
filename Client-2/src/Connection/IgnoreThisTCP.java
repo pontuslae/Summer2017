96487e6fe9c0ce39c244de5457880d0faa87a78f
Pontus <guslaepo@student.gu.se>
2017-06-21 17:42:17 -0400

    Adds mocking functionality

--- Client-2/src/Connection/IgnoreThisTCP.java
+++ Client-2/src/Connection/IgnoreThisTCP.java
@@ -2,20 +2,41 @@ package Connection;
 
 import External.Singleton;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.*;
 
 class IgnoreThisTCP {
 
+	ServerSocket serverSocket;
 
-	public void mock(String receive, String send){
-		System.out.println("Listening...");
+	IgnoreThisTCP() {
+
 		try {
-			Socket socket = new ServerSocket(9005).accept();
+			this.serverSocket = new ServerSocket(9005);
 		} catch (IOException ex) {
 			Singleton.debugPrint("Exception was thrown during mocking", ex);
 		}
-		System.out.println("Connected");
+	}
+
+
+	public void mock(String receive, byte send){
+		try {
+			Socket socket = serverSocket.accept();
+
+			BufferedReader inFromClient =
+					new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			DataOutputStream outToClient = new DataOutputStream(socket.getOutputStream());
+			String rl = inFromClient.readLine();
+
+			Singleton.debugPrint(rl);
+
+			outToClient.write(send);
+		} catch (IOException ex) {
+			Singleton.debugPrint("Exception was thrown during mocking", ex);
+		}
 	}
 
 	public static void main(String argv[]) throws Exception {
